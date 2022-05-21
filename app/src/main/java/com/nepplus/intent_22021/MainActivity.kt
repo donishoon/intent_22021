7f8cad81bca1106854fe4eaf8abaffbe8d51732f
강도훈 <fghfgh3255@gmail.com>
2022-05-21 14:34:13 +0900

    Intent로 메인에서 > 다른곳으로 이동

--- app/src/main/java/com/nepplus/intent_22021/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_22021/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nepplus.intent_22021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,7 +12,10 @@ class MainActivity : AppCompatActivity() {
 
         btnMove.setOnClickListener {
 
-//
+//            다른 화면으로 이동하기
+
+            val myintent = Intent (this, DoonActivity::class.java)
+            startActivity(myintent)
 
         }
 
