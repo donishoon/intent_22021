b7eca8b466738d595836c889cca3f8b0e2acf78d
강도훈 <fghfgh3255@gmail.com>
2022-05-21 14:11:05 +0900

    다른 화면으로 이동하기 준비

--- app/src/main/java/com/nepplus/intent_22021/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_22021/MainActivity.kt
@@ -2,10 +2,18 @@ package com.nepplus.intent_22021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMove.setOnClickListener {
+
+//            ㅁㄴㅇ
+
+        }
+
     }
 }
