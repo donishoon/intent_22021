5f3d0391e6453bb3f5ad7e550501d699b1967d4e
강도훈 <fghfgh3255@gmail.com>
2022-05-21 14:39:58 +0900

    finish() 기능으로 이전화면 복귀 -> 정확히는 현재화면이 종료 되는 거

--- app/src/main/java/com/nepplus/intent_22021/DoonActivity.kt
+++ app/src/main/java/com/nepplus/intent_22021/DoonActivity.kt
@@ -1,11 +1,25 @@
 package com.nepplus.intent_22021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_doon.*
 
 class DoonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doon)
+
+        btnMoveBack.setOnClickListener {
+
+//            다른 화면으로 이동하기
+
+//            val myIntent = Intent ( this, MainActivity :: class.java)
+//            startActivity(myIntent)
+
+            finish()
+
+        }
+
     }
 }
